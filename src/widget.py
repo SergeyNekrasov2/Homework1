1c42d04b04d24f806e8b36ce9dbd580d35dfc3d2
Sergey <nekrasov-nekras-nekrasov@mail.ru>
2024-08-10 14:16:33 +0300

    fixes for dev

--- src/widget.py
+++ src/widget.py
@@ -8,11 +8,11 @@ def mask_account_card(nums: str) -> str | None:
     else:
         cards = get_mask_card_number(nums[-16:])
         new_card = nums.replace(nums[-16:], cards)
-        return get_mask_card_number(nums)
+        return new_card
 
 
 # print(mask_account_card('73654108430135874305'))
-print(mask_account_card('Visa Platinum 7000792289606361'))
+# print(mask_account_card('Visa Platinum 7000792289606361'))
 # print(mask_account_card('Maestro 7000792289606361'))
 
 
@@ -21,3 +21,5 @@ def get_data(date: str) -> str | None:
     return f'{date[8:10]}.{date[5:7]}.{date[0:4]}'
 
 # print(get_data('2024-07-26T04:32:20.612820'))
+
+# изменения
