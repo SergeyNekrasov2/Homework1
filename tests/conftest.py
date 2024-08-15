7dc1c4357ee5d1b7c57dff4c422369dad6feed7e
Sergey <nekrasov-nekras-nekrasov@mail.ru>
2024-08-15 16:56:47 +0300

    conftest

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+@pytest.fixture
+def number_card(mask_number):
+    return '7000 79** **** 6361'
+
+
+@pytest.fixture
+def account():
+    return '**4305'
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,13 +1,14 @@
 from src.masks import get_mask_card_number, get_mask_account
 
 
-# @pytest.mark.parametrize("number_card, mask_number", [('1234567891234567', '1234 56** **** 4567'),
-#                                                      ('7000792289606361', '7000 79** **** 6361'),
-#                                                      ('2200240497191490', '2200 24** **** 1490'),])
+# @pytest.mark.parametrize("number_card, mask_number", [("1234567891234567", "1234 56** **** 4567"),
+#                                                       ("7000792289606361', '7000 79** **** 6361"),
+#                                                       ("2200240497191490', '2200 24** **** 1490"), ])
 def test_get_mask_card_number(number_card, mask_number):
     assert get_mask_card_number('7000792289606361') == '7000 79** **** 6361'
     assert get_mask_card_number('4324ji') == 'Некоректный номер карты. Введите 16-значный номер'
 
 
+# @pytest.mark.parametrize("account, mask_account", [("73654108430135874305", "**4305"), ])
 def test_get_mask_account(account, mask_account):
     assert get_mask_account('73654108430135874305') == '**4305'
