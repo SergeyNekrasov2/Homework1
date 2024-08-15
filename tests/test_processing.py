4c3e5dc154ef2ac0733e31ba3bab59987344d89e
Sergey <nekrasov-nekras-nekrasov@mail.ru>
2024-08-15 23:14:46 +0300

    test_processing

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -15,7 +15,7 @@ def test_filter_by_state_canceled(state):
 
 
 def test_sort_by_date(state):
-    """Тест для функции сортирующей исходные данные по дате, по возрастанию"""
+    """Тест для функции сортирующей исходные данные по дате,по возрастанию"""
     assert sort_by_date(state, False) == [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
                                           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
                                           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
@@ -23,7 +23,7 @@ def test_sort_by_date(state):
 
 
 def test_sort_by_date_reverse(state):
-    """Тест для функции сортирующей исходные данные по дате, по убыванию"""
+    """Тест для функции сортирующей исходные данные по дате,по убыванию"""
     assert sort_by_date(state, True) == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
                                          {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
                                          {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
