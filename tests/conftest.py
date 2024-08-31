e27918cea57fb2a4f0232b30f64f1d643048e210
Sergey <nekrasov-nekras-nekrasov@mail.ru>
2024-09-01 02:25:55 +0300

    utils

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 import pytest
 from src.generators import transactions
 
-
 @pytest.fixture
 def number_card(mask_number):
     return '7000 79** **** 6361'
@@ -33,3 +32,26 @@ def state():
 @pytest.fixture
 def test_transactions():
     return transactions
+
+@pytest.fixture
+def get_path():
+    return '../data/operations.json'
+
+
+@pytest.fixture
+def get_wrong_path():
+    return 'nothing'
+
+
+@pytest.fixture
+def get_bad_file():
+    return '../data/wrong_operations.json'
+
+@pytest.fixture
+def transactions():
+    return get_transactions_dictionary('../data/operations.json')
+
+
+@pytest.fixture
+def rub_transaction_number():
+    return 441945886
